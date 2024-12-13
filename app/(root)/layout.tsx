e5ccb7c34028bf5d9dac499c004976c35ce8292a
nermeenfares <nermen.morgan99@gmail.com>
2024-12-13 23:22:41 +0200

    feat:layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,17 @@
+import Footer from "@/components/shared/footer";
+import Navbar from "@/components/shared/navbar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  //test
   return (
     <html lang="en">
+      <Navbar />
       <body>{children}</body>
+      <Footer />
     </html>
   );
 }
