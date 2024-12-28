5f8eb3477727ad03909d1fcf88f8fad34f31868c
YasmineEl-shahat <yasmenelshahat754gmail.com>
2024-12-29 01:49:33 +0200

    feat: service card style

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -66,7 +66,7 @@ const Profile = () => {
           </TabsList>
 
           <TabsContent value="previous-services">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 mt-4">
               {services.map((service, index) => (
                 <ServiceCard
                   key={index}
--- components/cards/ServiceCard.tsx
+++ components/cards/ServiceCard.tsx
@@ -18,15 +18,20 @@ const ServiceCard: React.FC<ServiceCardProps> = ({
     <div className="border border-primary rounded-10 overflow-hidden">
       <Image
         src={imageSrc}
-        alt="Service"
-        width={250}
+        alt={title}
+        width={270}
         height={200}
         className="w-full"
       />
-      <article className="bg-lightGray p-2">
+      <article className="bg-lightGray p-2 text-darkGray text-sm h-full pb-4">
         <h3 className="font-semibold text-primary text-[20px]">{title}</h3>
-        <p>Warranty: {warranty}</p>
-        <p>Country of manufacture: {country}</p>
+        <p>
+          Warranty: <span className="text-subtitle">{warranty}</span>
+        </p>
+        <p>
+          Country of manufacture:{" "}
+          <span className="text-subtitle">{country}</span>
+        </p>
       </article>
     </div>
   );
