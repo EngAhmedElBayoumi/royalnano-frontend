17e4f55c10ca0af3effb29e471f9bff369537608
nermeenfares <nermen.morgan99@gmail.com>
2025-01-04 21:49:20 +0200

    map fixed

--- components/contactUs/ContactUs.tsx
+++ components/contactUs/ContactUs.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
-// import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 import ContactForm from "../forms/ContactForm";
-import MapComponent from "@/components/map/Map";
+// import MapComponent from "@/components/map/Map";
 // Dynamically Load the Map Component on the Client Side to solve ssr issue accessing window
-// const MapComponent = dynamic(() => import("../map/Map"), { ssr: false });
+const MapComponent = dynamic(() => import("../map/Map"), { ssr: false });
 
 const ContactUs = () => {
   return (
