c2fc43c7a599dfe384b8dd51c5a7179d7df60452
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-01-05 09:34:21 +0200

    fix: subscription gap

--- components/Subscription.tsx
+++ components/Subscription.tsx
@@ -26,7 +26,7 @@ const Subscription = () => {
 
   return (
     <section className="py-5 md:pt-0 sm:pb-0 bg-gray300 text-black flex justify-between items-center mb-8">
-      <article className="px-4 md:pl-[50px] w-full md:w-auto">
+      <article className="px-4 sm:pl-[120px] w-full md:w-auto">
         <h2 className="text-md lg:text-lg xl:text-xl mb-4 max-w-[530px] lg:w-2/3">
           Subscribe to get the latest car maintenance updates
         </h2>
