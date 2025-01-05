e5b14a4ce7f06344e2c81759f10e98323b543b20
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-01-05 03:34:27 +0200

    feat: swiper thumbnail

--- components/Gallery/GalleryItemModal.tsx
+++ components/Gallery/GalleryItemModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   Dialog,
   DialogContent,
@@ -12,6 +12,7 @@ import "swiper/css/navigation";
 import Image from "next/image";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { Navigation } from "swiper/modules";
+import type { Swiper as SwiperType } from "swiper";
 
 interface GalleryItem {
   type: string;
@@ -30,9 +31,12 @@ const GalleryItemModal: React.FC<GalleryItemModalProps> = ({
   onClose,
   item,
 }) => {
+  const [selectedIndex, setSelectedIndex] = React.useState(0);
+  const swiperRef = useRef<SwiperType | null>(null);
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="sm:max-w-[800px] h-[80vh] bg-[transparent] border-none">
+      <DialogContent className="sm:max-w-[700px] h-[100vh] bg-[transparent] border-none">
         <DialogHeader>
           <VisuallyHidden>
             <DialogTitle>
@@ -41,31 +45,53 @@ const GalleryItemModal: React.FC<GalleryItemModalProps> = ({
           </VisuallyHidden>
         </DialogHeader>
         {item.type === "image" ? (
-          <Swiper
-            className="w-full h-full"
-            navigation={{
-              nextEl: ".swiper-button-next",
-              prevEl: ".swiper-button-prev",
-            }}
-            modules={[Navigation]}
-          >
-            {item.images.map((image, index) => (
-              <SwiperSlide
-                key={index}
-                className="flex justify-center w-full h-full"
-              >
+          <>
+            <Swiper
+              onSwiper={(swiper) => (swiperRef.current = swiper)} // Assign swiper instance
+              className="w-full h-full"
+              navigation={{
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+              }}
+              onSlideChange={(swiper) => setSelectedIndex(swiper.activeIndex)}
+              modules={[Navigation]}
+            >
+              {item.images.map((image, index) => (
+                <SwiperSlide
+                  key={index}
+                  className="flex justify-center w-full h-full"
+                >
+                  <Image
+                    src={image}
+                    alt={`Gallery Image ${index + 1}`}
+                    width={400}
+                    height={400}
+                    className="w-full h-full object-cover rounded-lg"
+                  />
+                </SwiperSlide>
+              ))}
+              <div className="swiper-button-prev" />
+              <div className="swiper-button-next" />
+            </Swiper>
+            <div className="flex justify-center mt-4">
+              {item.images.map((image, index) => (
                 <Image
+                  key={index}
                   src={image}
-                  alt={`Gallery Image ${index + 1}`}
-                  width={400}
-                  height={400}
-                  className="w-full h-full object-cover rounded-lg"
+                  alt={`Thumbnail ${index + 1}`}
+                  width={100}
+                  height={100}
+                  className={`cursor-pointer rounded-lg mx-1 ${
+                    selectedIndex === index ? "border-2 border-primary" : ""
+                  }`}
+                  onClick={() => {
+                    setSelectedIndex(index);
+                    swiperRef.current?.slideTo(index); // Use the swiper instance to slide
+                  }}
                 />
-              </SwiperSlide>
-            ))}
-            <div className="swiper-button-prev border-primary" />
-            <div className="swiper-button-next border-primary" />
-          </Swiper>
+              ))}
+            </div>
+          </>
         ) : (
           <video controls className="w-full h-full">
             <source src={item.videoSrc} type="video/mp4" />
