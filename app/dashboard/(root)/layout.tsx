acfccbc950dbf536dac9b6fe36ab3ce563491e40
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-01-09 05:08:57 +0200

    feat: dashboard layout

---
+++ app/dashboard/(root)/layout.tsx
@@ -0,0 +1,31 @@
+import { Cairo } from "next/font/google";
+import "../../globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/dashboard/AppSidebar/AppSidebar";
+
+const cairo = Cairo({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Royal Nano Dashboard",
+  description: "Royal Nano Erp",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cairo.className}>
+        <SidebarProvider>
+          <AppSidebar />
+          <main className="main">
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
---
+++ components/dashboard/AppSidebar/AppSidebar.tsx
@@ -0,0 +1,93 @@
+"use client";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import Image from "next/image";
+import { sidebarLinks } from "@/data/dashboard/sidebarData";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export function AppSidebar() {
+  const currentPath = usePathname();
+  console.log(`/dashboard/${sidebarLinks[0].path}`);
+
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader className="items-center">
+        <Image src="/assets/icons/logo.svg" alt="logo" width={50} height={80} />
+      </SidebarHeader>
+      <SidebarContent className="mt-12">
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu className="gap-2">
+              {sidebarLinks.map((link) => {
+                const isActive = `/dashboard${link.path}` === currentPath + "/";
+
+                return (
+                  <SidebarMenuItem
+                    key={link.path}
+                    className="flex justify-center"
+                  >
+                    <SidebarMenuButton
+                      asChild
+                      isActive={isActive}
+                      className="py-6 !rounded-10"
+                    >
+                      <Link href={`/dashboard${link.path}`}>
+                        <Image
+                          src={`/assets/icons/sidebar/${link.icon}`}
+                          alt={link.name}
+                          width={30}
+                          height={30}
+                        />
+                        <span className="text-primary">{link.name}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenuButton
+              asChild
+              isActive={"/dashboard/settings" === currentPath}
+              className="py-6 !rounded-10"
+            >
+              <Link href="/dashboard/settings">
+                <Image
+                  src={`/assets/icons/sidebar/setting.svg`}
+                  alt="settings"
+                  width={30}
+                  height={30}
+                />
+                <span className="text-primary">settings</span>
+              </Link>
+            </SidebarMenuButton>
+            <SidebarMenuButton className="py-6 !rounded-10 !bg-transparent">
+              <Image
+                src={`/assets/icons/sidebar/logout.svg`}
+                alt="settings"
+                width={30}
+                height={30}
+              />
+              <span className="text-primary">logout</span>
+            </SidebarMenuButton>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
