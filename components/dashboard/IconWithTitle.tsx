6802e0433190e3c15e9dbd62ecd98a20a32925c9
nermeenfares <nermen.morgan99@gmail.com>
2025-01-11 20:49:37 +0200

    clients

---
+++ components/dashboard/IconWithTitle.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+interface IconWithTitleProps {
+  imageSrc: string; 
+  title: string;   
+  backgroundColor: string; 
+  textColor: string;      
+}
+
+function IconWithTitle({ imageSrc, title, backgroundColor, textColor  }: IconWithTitleProps) {
+  return (
+    <div
+    style={{ backgroundColor: backgroundColor }}
+    className={`flex py-[18px] px-[34px] gap-1.5`}>
+      <img src={imageSrc} alt={title} className="w-6 h-6" /> 
+      <p className={`text-[20px] text-${textColor}`}>{title}</p>
+    </div>
+  )
+}
+
+export default IconWithTitle;
