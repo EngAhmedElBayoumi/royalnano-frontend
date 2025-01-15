97e937acaf96f7d5fb4f8dffea17410ef7d3e9a5
nermeenfares <nermen.morgan99@gmail.com>
2025-01-15 23:38:05 +0200

    emptyMessage

--- components/dashboard/EmptyMessage.tsx
+++ components/dashboard/EmptyMessage.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 function EmptyMessage() {
   return (
-    <div className="bg-dashboardBg px-4 pt-4 pb-1 rounded-tr-[20px] rounded-bl-[20px] flex-col rounded-br-[20px] flex justify-center    card gap-[80px]">
-<Image height={400} width={400} alt='img' src='/assets/icons/noCustomers.svg'/>
+    <div className="bg-dashboardBg pt-20 px-4 pt-4 pb-1 rounded-tr-[20px] rounded-bl-[20px] flex-col rounded-br-[20px] flex justify-center items-center w-full    card gap-15">
+<Image height={100} width={400} alt='img' src='/assets/icons/noCustomers.svg'/>
 <p className='text-[30px] text-[#7F7F7F] font-[500]'>You dont have any clients.</p>
       
     </div>
