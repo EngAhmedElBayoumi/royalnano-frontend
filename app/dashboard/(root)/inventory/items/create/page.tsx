dcaec11b6894970e8d40bb336f34cc9325bd9980
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-01-15 21:31:28 +0200

    feat: inventory items form

---
+++ app/dashboard/(root)/inventory/items/create/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import ItemForm, {
+  ItemFormValues,
+} from "@/components/dashboard/forms/inventory/ItemForm";
+import IconWithTitle from "@/components/dashboard/IconWithTitle";
+// import { useCreateItemMutation } from "@/redux/services/InventoryApi";
+
+export default function CreateItem() {
+  // const [createItem] = useCreateItemMutation();
+
+  const handleSubmit = async (data: ItemFormValues) => {
+    console.log(data);
+    // await createItem(data);
+  };
+
+  return (
+    <main className="mx-7 my-5">
+      <div className="flex">
+        <IconWithTitle
+          imageSrc="/assets/icons/add.svg"
+          title="Add Item"
+          backgroundColor="#F8F7F7"
+          textColor="primary"
+        />
+      </div>
+
+      <div className="bg-[#F8F7F7] px-6 pt-5 pb-8 rounded-r-[20px] rounded-bl-[20px] lg:pr-[200px]">
+        <ItemForm onSubmit={handleSubmit} />
+      </div>
+    </main>
+  );
+}
