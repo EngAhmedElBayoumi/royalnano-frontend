4a40a1ca7fc091c5c3841a069ac0d82f052e6eaf
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-01-25 04:27:59 +0200

    layout meta data

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,11 @@
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/MainNavbar";
 
+export const metadata = {
+  title: "Royal Nano",
+  description: "Royal Nano Erp",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
