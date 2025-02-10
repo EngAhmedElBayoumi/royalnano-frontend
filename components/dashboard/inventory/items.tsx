99800f5171a8375ab26439a5b98cc869271d7f4d
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-02-10 03:34:09 +0200

    feat: error loading table data

--- components/dashboard/inventory/items.tsx
+++ components/dashboard/inventory/items.tsx
@@ -1,7 +1,8 @@
 "use client";
-import CustomTable from "@/components/dashboard/tables/CustomTable";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useGetItemsQuery } from "@/redux/services/dashboard/itemsApi";
+import CustomTable from "@/components/dashboard/tables/CustomTable";
 import TableSkelton from "../skelton/TableSkelton";
 import CardsSkelton from "../skelton/CardsSkelton";
 
@@ -44,6 +45,16 @@ export default function Items() {
         <div className="bg-dashboardBg px-4 pt-4 pb-1 rounded-tr-[20px] rounded-bl-[20px] rounded-br-[20px] card mb-5 ">
           <CardsSkelton />
           <TableSkelton />
+        </div>
+      ) : error ? (
+        <div className="flex justify-center flex-col items-center bg-dashboardBg pb-10 rounded-tr-[20px] rounded-bl-[20px] rounded-br-[20px] card mb-5">
+          <Image
+            src="/assets/icons/dashboard/loading-error.svg"
+            alt="loading error"
+            width="400"
+            height="300"
+          />
+          Error loading data
         </div>
       ) : (
         <CustomTable
