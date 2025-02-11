855c1390d890b74fd953c0307f04ad54c26c60df
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-02-11 04:07:06 +0200

    feat: stock adjustment schema

--- lib/validations/dashboard/inventory/stockAdjustmentSchema.ts
+++ lib/validations/dashboard/inventory/stockAdjustmentSchema.ts
@@ -1,9 +1,12 @@
 import * as z from "zod";
 
 export const stockAdjustmentSchema = z.object({
+  item: z.coerce.number().min(1, "Item must be selected"),
   reason: z.string().nonempty("Reason is required"),
-  item: z.string().nonempty("Item is required"),
-  quantity: z.number().min(0, "Quantity must be a non-negative number"),
-  type: z.string().nonempty("Type is required"),
-  date: z.date({ required_error: "Date is required" }),
+  quantity_adjusted: z
+    .number()
+    .min(0, "Quantity must be a non-negative number"),
+  adjustment_type: z.enum(["Increase", "Decrease"]),
+  adjustment_date: z.date({ required_error: "Date is required" }),
+  description: z.string().nonempty("Description is required"),
 });
