6281486ce0d42703f2eced4d8df961be4cc2388a
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-02-25 07:41:24 +0200

    feat: forbidden page

--- middleware.ts
+++ middleware.ts
@@ -61,9 +61,9 @@ export async function middleware(req: NextRequest) {
         // Check if user has at least one of the required permissions
         const hasPermission = userPermissions.includes(requiredPermissions);
 
-        if (!hasPermission) {
+        if (hasPermission) {
           // Redirect to unauthorized page or dashboard
-          return NextResponse.redirect(new URL("/unauthorized", req.url));
+          return NextResponse.redirect(new URL("/en/forbidden", req.url));
         }
       }
     }
