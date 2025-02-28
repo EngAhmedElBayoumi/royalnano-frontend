8f48d4435e811896c334a75669d649165e2c0dc6
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-02-28 03:35:13 +0200

    fix: disabled issue

--- components/formFields/DatePicker.tsx
+++ components/formFields/DatePicker.tsx
@@ -50,8 +50,9 @@ const DatePicker = <T extends FieldValues>({
           </FormLabel>
           <Popover>
             <PopoverTrigger asChild>
-              <FormControl disabled={readonly}>
+              <FormControl>
                 <Button
+                  disabled={readonly}
                   variant={"outline"}
                   className={cn(
                     "bg-[#F4F4F4] border-gray xl:rounded-10 px-2 py-5 xl:py-7",
