e5142fedfcf04278b20f12aa720c2ec319f9efc0
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-03-02 11:17:44 +0200

    feat: update bonuses

--- app/[locale]/dashboard/(root)/hr/bonuses/edit/page.tsx
+++ app/[locale]/dashboard/(root)/hr/bonuses/edit/page.tsx
@@ -1,28 +1,61 @@
 "use client";
+import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useTranslations } from "next-intl";
+import {
+  useUpdateBonusMutation,
+  useGetBonusByIdQuery,
+} from "@/redux/services/dashboard/hr/bonusesApi";
+import IconWithTitle from "@/components/dashboard/IconWithTitle";
+import CustomModal from "@/components/modals/CustomModal";
+import FormSkelton from "@/components/dashboard/skelton/FormSkelton";
+import LoadingError from "@/components/dashboard/LoadingError";
 import BonusesForm, {
   BonusesFormValues,
 } from "@/components/dashboard/forms/hr/BonusesForm";
-import IconWithTitle from "@/components/dashboard/IconWithTitle";
-// import { useUpdateBonusMutation } from "@/redux/services/BonusApi";
 
 export default function EditBonus() {
-  // const [updateBonus] = useUpdateBonusMutation();
-  const defaultValues: BonusesFormValues = {
-    name: "John Doe",
-    branch_name: "Branch 1",
-    rewards: "1000",
-    start: new Date(),
-    end: new Date(new Date().setDate(new Date().getDate() + 1)),
-    date: new Date(),
-  }; // Fetch existing bonus data and set as default values
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+  const t = useTranslations("hr");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { data, isLoading, error } = useGetBonusByIdQuery(id);
+  const [updateBonus] = useUpdateBonusMutation();
 
+  const defaultValues: BonusesFormValues = data && {
+    ...data,
+    employee: Number(data.employee),
+  };
+
+  const handleModalChange = (isOpen: boolean) => {
+    setIsModalOpen(isOpen);
+  };
   const handleSubmit = async (data: BonusesFormValues) => {
-    console.log(data);
-    // await updateBonus(data);
+    try {
+      const payload = {
+        ...data,
+        employee: Number(data.employee),
+      };
+
+      const response = await updateBonus({ id, data: payload });
+
+      if (response.error) throw new Error("edit failed");
+      else router.push(`/dashboard/hr?tab=${t("tabs.bonuses")}`);
+    } catch (error) {
+      setIsModalOpen(true);
+      console.log(error);
+    }
   };
 
   return (
     <main className="mx-7 my-5">
+      <CustomModal
+        isOpen={isModalOpen}
+        onChange={handleModalChange}
+        title="Error!"
+        description="Your Request wasn't processed successfully.."
+      />
       <div className="flex">
         <IconWithTitle
           imageSrc="/assets/icons/edit.svg"
@@ -33,7 +66,13 @@ export default function EditBonus() {
       </div>
 
       <div className="bg-dashboardBg px-6 pt-5 pb-8 ltr:rounded-r-[20px] ltr:rounded-bl-[20px] rtl:rounded-l-[20px] rtl:rounded-br-[20px] ltr:lg:pr-[200px] rtl:lg:pl-[200px]">
-        <BonusesForm onSubmit={handleSubmit} defaultValues={defaultValues} />
+        {isLoading ? (
+          <FormSkelton />
+        ) : error ? (
+          <LoadingError />
+        ) : (
+          <BonusesForm onSubmit={handleSubmit} defaultValues={defaultValues} />
+        )}
       </div>
     </main>
   );
