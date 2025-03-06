eace2fdca6272dbf3d966ad87d9326993b41fff3
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-03-06 02:23:04 +0200

    feat: applicants api

---
+++ redux/services/dashboard/hr/applicantsApi.ts
@@ -0,0 +1,39 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { baseQuery } from "../../common";
+
+export const applicantsApi = createApi({
+  reducerPath: "applicantsApi",
+  baseQuery,
+  endpoints: (builder) => ({
+    getApplicants: builder.query({
+      query: ({ search, ordering, page, page_size }) => ({
+        url: `hr/applicants`,
+        params: { search, ordering, page, page_size },
+      }),
+    }),
+    getApplicantById: builder.query({
+      query: (id) => `hr/applicants/${id}/`,
+    }),
+    createApplicant: builder.mutation({
+      query: (data) => ({
+        url: `hr/applicants/`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    updateApplicant: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `hr/applicants/${id}/`,
+        method: "PATCH",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetApplicantsQuery,
+  useGetApplicantByIdQuery,
+  useCreateApplicantMutation,
+  useUpdateApplicantMutation,
+} = applicantsApi;
