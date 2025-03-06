2888751d51dfcd175b9e7951ebd448f48e0184f0
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-03-06 19:50:43 +0200

    fix: remove salaries comp

--- lib/validations/dashboard/hr/salarySchema.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod";
-
-export const salarySchema = z.object({
-  name: z.string().nonempty("Name is required"),
-  job_title: z.string().nonempty("Job title is required"),
-  salary: z.string().nonempty("Salary is required"),
-  date: z.date({ required_error: "Date is required" }),
-});
