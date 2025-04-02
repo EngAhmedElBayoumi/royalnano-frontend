135b846ba150d4dc1cfa5399eba13f01463c5d38
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-04-02 12:51:00 +0200

    refactor(jobsSchema): update permissions validation to use coercion and improve error messaging

--- lib/validations/dashboard/hr/jobsSchema.ts
+++ lib/validations/dashboard/hr/jobsSchema.ts
@@ -1,9 +1,8 @@
-
-import { z } from 'zod';
+import { z } from "zod";
 
 export const jobsSchema = z.object({
   name: z.string().min(1, "Name cannot be empty"),
-  permissions: z.array(z.number().int())
-    .nonempty("Permissions array cannot be empty")
-    .min(1, "At least one permission is required"),
+  permissions: z.array(
+    z.coerce.number().min(1, "At least one permission is required")
+  ),
 });
