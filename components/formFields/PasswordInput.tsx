981526db68bd9464cea8839701fb59ca88fce81e
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-04-14 09:44:36 +0200

    fix(PasswordInput): update button class for RTL support

--- components/formFields/PasswordInput.tsx
+++ components/formFields/PasswordInput.tsx
@@ -53,7 +53,7 @@ const PasswordInput = <T extends FieldValues>({
               type="button"
               variant="ghost"
               size="sm"
-              className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
+              className="absolute ltr:right-0 rtl:left-0 top-0 h-full px-3 py-2 hover:bg-transparent"
               onClick={() => setShowPassword((prev) => !prev)}
             >
               {showPassword ? (
