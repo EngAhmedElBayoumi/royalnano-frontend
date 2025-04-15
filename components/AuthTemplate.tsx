fae54cd641fb6519e9eeed476349135fab3da271
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-04-15 19:37:38 +0200

    feat(AuthTemplate): wrap logo image in a link for better navigation

--- components/AuthTemplate.tsx
+++ components/AuthTemplate.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 const AuthTemplate = ({
   src,
   Form,
@@ -12,13 +13,15 @@ const AuthTemplate = ({
       <main className="py-2 bg-neutralGray lg:h-[94vh] flex flex-col lg:flex-row min-w-[280px] w-[90%] sm:w-[80%] rounded-tl-[100px] rounded-br-[100px]">
         {/* left side */}
         <div className="flex flex-col justify-center items-center w-[100%] h-[100%]">
-          <Image
-            src="/assets/icons/logo.svg"
-            alt="logo"
-            width={90}
-            height={122}
-            className="mb-10"
-          />
+          <Link href="/">
+            <Image
+              src="/assets/icons/logo.svg"
+              alt="logo"
+              width={90}
+              height={122}
+              className="mb-10"
+            />
+          </Link>
           <div className="flex text-sm lg:text-[20px] xl:text-xl font-[400] gap-1">
             <p className="text-nowrap">Welcome To</p>
             <span className="text-primary">Royal</span>
