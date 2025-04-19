29192f1c82ed269aaee7271731185e57090f37a7
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-04-19 14:14:47 +0200

    fix: improve loading and error handling in EditSalesCustomer component

--- app/[locale]/dashboard/(root)/sales/sales-customer/edit/page.tsx
+++ app/[locale]/dashboard/(root)/sales/sales-customer/edit/page.tsx
@@ -7,6 +7,8 @@ import { useEffect, useState } from "react";
 import { useGetSalesCustomerByIdQuery } from "@/redux/services/dashboard/sales/salesCustomerApi";
 import { SalesCustomerFormValues } from "@/lib/validations/dashboard/sales/salesCustomerSchema";
 import EditSalesCustomerForm from "@/components/dashboard/forms/sales/editSalesCustomerForm";
+import FormSkelton from "@/components/dashboard/skelton/FormSkelton";
+import LoadingError from "@/components/dashboard/LoadingError";
 
 export default function EditSalesCustomer() {
   const t = useTranslations("Sales");
@@ -20,7 +22,7 @@ export default function EditSalesCustomer() {
   const {
     data: customerData,
     isLoading,
-    isError,
+    error,
   } = useGetSalesCustomerByIdQuery(id);
 
   useEffect(() => {
@@ -42,14 +44,6 @@ export default function EditSalesCustomer() {
     }
   }, [customerData]);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <div>Error loading customer data.</div>;
-  }
-
   return (
     <main className="mx-4 sm:mx-7 my-5">
       <div className="flex">
@@ -62,8 +56,14 @@ export default function EditSalesCustomer() {
       </div>
 
       <div className="bg-dashboardBg px-4 sm:px-6 pt-5 pb-8 ltr:rounded-r-[20px] ltr:rounded-bl-[20px] rtl:rounded-l-[20px] rtl:rounded-br-[20px] ltr:lg:pr-[200px] rtl:lg:pl-[200px]">
-        {defaultValues && (
-          <EditSalesCustomerForm defaultValues={defaultValues} />
+        {isLoading ? (
+          <FormSkelton />
+        ) : error ? (
+          <LoadingError />
+        ) : (
+          defaultValues &&
+          !isLoading &&
+          !error && <EditSalesCustomerForm defaultValues={defaultValues} />
         )}
       </div>
     </main>
