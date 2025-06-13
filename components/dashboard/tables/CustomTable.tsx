8a10bef6bf0c3de09c4316c2b021ce0010326b83
YasmineEl-shahat <yasmenelshahat754gmail.com>
2025-06-13 21:25:36 +0300

    refactor: remove commented-out Column component code in CustomTable

--- components/dashboard/tables/CustomTable.tsx
+++ components/dashboard/tables/CustomTable.tsx
@@ -130,17 +130,6 @@ const CustomTable = ({
             }
           >
             {visibleColumns.map((col) => (
-              // <Column
-              //   key={col.field}
-              //   field={col.field}
-              //   header={col.header}
-              //   headerStyle={{ backgroundColor: "#C8AE50" }}
-              //   className="rtl:text-right m-auto py-[13px] px-[38px] text-[14px] font-[500] border-r border-white border-[2px]"
-              //   headerClassName="text-center capitalize text-white text-[16px] font-[500] py-[13px] px-[38px] border-r border-white border-[2px]"
-              //   body={(rowData) =>
-              //     renderColumnBody({ col, rowData, expandedRows, toggleExpand })
-              //   }
-              // />
               <Column
                 key={col.field}
                 field={col.field}
